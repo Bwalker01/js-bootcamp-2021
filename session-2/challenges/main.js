6ed478e56aec7ce089f7a0b45aa0f0e5cfd8093a
Ben <benjaminjwalker2910@gmail.com>
2021-09-23 15:13:20 +0100

    more notes to session 2

--- session-2/challenges/main.js
+++ session-2/challenges/main.js
@@ -1,1 +1,13 @@
-document.getElementById("dateNow").innerHTML = new Date();
+console.log("hello world");
+console.log("prettier");
+let variable = "string";
+let twelve = 11;
+console.log(`Twleve is not ${twelve}`);
+twelve++;
+console.log(`Twelve is ${twelve}`);
+
+let myFunc = function () {
+  console.log("This is in a function");
+};
+
+myFunc();
