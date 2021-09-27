482a64e372e7a79acf74421c5df932ecfb4f98d1
Ben <benjaminjwalker2910@gmail.com>
2021-09-27 14:31:30 +0100

    update

--- session-3/challenges/divisibleBy.js
+++ session-3/challenges/divisibleBy.js
@@ -1,11 +1,11 @@
 let total = 0;
 for (let i = 1; i < 501; i++) {
   if (i % 3 === 0 || i % 5 === 0) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log(i, "***");
-    } else {
-      console.log(i);
-    }
+    // if (i % 3 === 0 && i % 5 === 0) {
+    //   console.log(i, "***");
+    // } else {
+    console.log(i);
+    // }
     total += i;
   }
 }
