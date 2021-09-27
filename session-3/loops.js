154ee90f29e5bbda1e9760950a42b5b183682f66
Ben <benjaminjwalker2910@gmail.com>
2021-09-27 13:58:48 +0100

    having a function for doubling numbers

--- session-3/loops.js
+++ session-3/loops.js
@@ -6,3 +6,11 @@ console.log(isHappy);
 for (let i = 0; i < 100; i++) {
   console.log(i);
 }
+
+const nums = [1, 2, 3, 4, 5];
+for (let i = 0; i < nums.length; i++) {
+  console.log(nums);
+  nums[i] = nums[i] * 2;
+}
+
+console.log(nums);
