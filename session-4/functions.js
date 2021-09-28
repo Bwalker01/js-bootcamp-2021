f0a96a6071b9336e78904ec253704a86a35498d2
Ben <benjaminjwalker2910@gmail.com>
2021-09-28 16:09:20 +0100

    function notes

--- session-4/functions.js
+++ session-4/functions.js
@@ -25,3 +25,46 @@ oneMoreMessage();
 
 // HOISTING - Holds onto any function so it can be invoked even before its assignment
 //This only works for declared functions and NOT function expressions
+
+//Immediate invoke
+
+(function () {
+  console.log("This anonymous function will immediately run.");
+})();
+
+//With Parameters
+let fName = "Ben";
+
+(function (name) {
+  console.log("Hello there,", name, ".");
+})(fName);
+
+//Parameters is what variables the declaration states the function takes
+//Arguments are what is passed to the function when its invoked
+
+function addFive(num) {
+  return num + 5; //Return the value stated into a variable then the function is invoked
+}
+console.log(addFive(3));
+
+// ---OR---
+
+addFour = (num) => num + 4;
+console.log(addFour(3));
+
+const formatPerson = (firstName, lastName, age, dob, address, hobbies) => {
+  //This is too long
+  console.log("");
+};
+//The parameters should be consolidated in an object
+//-->
+const formattedPerson = (personObj) => {
+  const { firstName, lastName, age, dob, address, hobbies } = personObj;
+  return {
+    name: `${firstName} ${lastName}`,
+  };
+};
+
+//SRP --Single Responsibility Principle (Only write a function to do one thing and keep functions seperate)
+//KISS --Keep It Simple Stupid (Simplify the code as much as possible)
+//DRY --Don't Repeat Yourself (Use functions rather than writing the same code repeatedly)
