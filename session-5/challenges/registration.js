de199a4829fb8c97f96062ca20eeb8429e37f02b
Ben <benjaminjwalker2910@gmail.com>
2021-09-30 11:45:49 +0100

    updated to handle lowercase and non strings

--- session-5/challenges/registration.js
+++ session-5/challenges/registration.js
@@ -1,30 +1,35 @@
 customRegistration = (registration) => {
-  if (registration.length === 8 && registration.includes(" ")) {
-    regAsArray = registration.split("");
-    for (let i = 1; i < regAsArray.length; i++) {
-      switch (regAsArray[i]) {
-        case "A":
-          regAsArray[i] = 4;
-          break;
-        case "B":
-          regAsArray[i] = 8;
-          break;
-        case "S":
-          regAsArray[i] = 5;
-          break;
-        case "E":
-          regAsArray[i] = 3;
-          break;
+  if (typeof registration === "string") {
+    registration = registration.toUpperCase();
+    if (registration.length === 8 && registration.includes(" ")) {
+      regAsArray = registration.split("");
+      for (let i = 1; i < regAsArray.length; i++) {
+        switch (regAsArray[i]) {
+          case "A":
+            regAsArray[i] = 4;
+            break;
+          case "B":
+            regAsArray[i] = 8;
+            break;
+          case "S":
+            regAsArray[i] = 5;
+            break;
+          case "E":
+            regAsArray[i] = 3;
+            break;
+        }
       }
+      formattedRegistration = regAsArray.join("");
+      return formattedRegistration;
+    } else {
+      return "Registration must contain 8 characters with a space";
     }
-    formattedRegistration = regAsArray.join("");
-    return formattedRegistration;
   } else {
-    return "Registration must contain 8 characters with a space";
+    return "Registration must be a string";
   }
 };
 
-const registration = "ABA KMEV";
+const registration = "atl kjah";
 formattedRegistration = customRegistration(registration);
 
 console.log(formattedRegistration);
