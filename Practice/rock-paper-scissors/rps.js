f1dbd1021b2a61c5e66de61abc4a7733ba2afb71
Ben <benjaminjwalker2910@gmail.com>
2021-09-30 09:36:19 +0100

    creating the better rps program

--- Practice/rock-paper-scissors/rps.js
+++ Practice/rock-paper-scissors/rps.js
@@ -2,10 +2,37 @@ function play(userChoice) {
   const options = ["Rock", "Paper", "Scissors"];
   const compChoice = options[Math.floor(Math.random() * 3)];
 
+  document.getElementById("inp1").disabled = true;
+  document.getElementById("inp2").disabled = true;
+  document.getElementById("inp3").disabled = true;
   console.log("User: ", userChoice);
-  document.getElementById("users").innerHTML = userChoice;
+  if (document.getElementById("u")) {
+    ani = document
+      .getElementById("u")
+      .animate([{ opacity: 1 }, { opacity: 0 }], 500);
+    ani.onfinish = () => {
+      return false;
+    };
+    while (ani.onfinish()) {}
+  }
+  document.getElementById(
+    "users"
+  ).innerHTML = `<img src='${userChoice}.png' class="center" id="u">`;
+  document.getElementById("u").animate([{ opacity: 0 }, { opacity: 1 }], 500);
   console.log("Computer: ", compChoice);
-  document.getElementById("comps").innerHTML = compChoice;
+  if (document.getElementById("c")) {
+    ani = document
+      .getElementById("c")
+      .animate([{ opacity: 1 }, { opacity: 0 }], 500);
+    ani.onfinish = () => {
+      return false;
+    };
+    while (ani.onfinish()) {}
+  }
+  document.getElementById(
+    "comps"
+  ).innerHTML = `<img src='${compChoice}.png' class="center" id="c">`;
+  document.getElementById("c").animate([{ opacity: 0 }, { opacity: 1 }], 500);
   if (userChoice === compChoice) {
     console.log("Draw");
     document.getElementById("winner").innerHTML = "Draw!";
@@ -20,10 +47,13 @@ function play(userChoice) {
     document.getElementById("userScore").innerText = userScore;
     console.log("User wins");
   } else {
-    document.getElementById("winner").innerHTML = "I win!";
+    document.getElementById("winner").innerHTML = "Comp wins!";
     let compScore = document.getElementById("compScore").innerText;
     compScore++;
     document.getElementById("compScore").innerText = compScore;
     console.log("Computer wins");
   }
+  document.getElementById("inp1").disabled = false;
+  document.getElementById("inp2").disabled = false;
+  document.getElementById("inp3").disabled = false;
 }
