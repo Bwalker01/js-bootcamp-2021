901a99f422476b284e5a9a2b9a9222cdfab09f23
Ben <benjaminjwalker2910@gmail.com>
2021-09-30 10:46:04 +0100

    more functions

--- session-4/functions.js
+++ session-4/functions.js
@@ -62,9 +62,87 @@ const formattedPerson = (personObj) => {
   const { firstName, lastName, age, dob, address, hobbies } = personObj;
   return {
     name: `${firstName} ${lastName}`,
+    city: address.city,
+    hobbies: hobbies,
   };
 };
+
+person = {
+  firstName: "Ben",
+  lastName: "Walker",
+  age: 18,
+  dob: "29/10/2002",
+  address: {
+    street: "1 Main Street",
+    city: "London",
+    postCode: "L1 5TB",
+  },
+  hobbies: ["Programming", "Eating", "Watching Netflix"],
+};
+relative = formattedPerson(person);
+console.log(relative);
 
 //SRP --Single Responsibility Principle (Only write a function to do one thing and keep functions seperate)
 //KISS --Keep It Simple Stupid (Simplify the code as much as possible)
 //DRY --Don't Repeat Yourself (Use functions rather than writing the same code repeatedly)
+
+//========= EXTRA ========
+//Declaration (Global)
+function debitAccount(account, amount) {
+  account.total += amount;
+  return account;
+}
+
+//Invoke
+const account = {
+  total: 0,
+};
+const amount = 100;
+
+const updatedAccount = debitAccount(account, amount);
+console.log(updatedAccount);
+
+//Function Expression
+const greet = function (message) {
+  return `Hello ${message}`;
+};
+
+console.log(greet("loser"));
+
+//Function Expression as Arrow
+const messageRenderer = (message) => `Hello ${message}`;
+
+console.log(messageRenderer(messageRenderer("Hello")));
+
+//Functions with default values
+const makeCoffee = (
+  coffeeType = "americano",
+  coffeeOptions = {
+    hasMilk: false,
+    hasSugar: true,
+    toppings: [],
+  }
+) => {
+  console.log(
+    `Coffee ${coffeeType}, Coffee Options ${JSON.stringify(coffeeOptions)}`
+  );
+  let coffeeMessage = "";
+  if (coffeeType === "cappucino" || coffeeType === "americano") {
+    coffeeOptions.toppings.forEach(function (topping) {
+      coffeeMessage += `added ${topping}, `;
+    });
+    coffeeColour = coffeeOptions.hasMilk ? "" : "black ";
+    coffeeMessage += `Enjoy your ${coffeeColour}${coffeeType}`;
+  } else {
+    coffeeMessage = "Sorry we do not sell" + coffeeType;
+  }
+  return coffeeMessage;
+};
+
+const coffeeType = "cappucino";
+const coffeeOptions = {
+  toppings: ["vanilla", "cream"],
+  hasMilk: true,
+};
+
+console.log(makeCoffee());
