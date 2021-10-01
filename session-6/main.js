00296c6f5331b6abccab1687756cf3a71af62b61
Ben <benjaminjwalker2910@gmail.com>
2021-10-01 09:55:37 +0100

    callbacks

--- session-6/main.js
+++ session-6/main.js
@@ -3,4 +3,31 @@ console.log("Session 6");
 //Callback Functions
 const people = ["John", "Chris", "Ben", "Lauren", "Phil"];
 
-people.forEach();
+const looper = (argument) => {
+  console.log("arg =>", argument);
+};
+
+// people.forEach(looper);
+
+//Example
+
+const makeDrink = (callback) => {
+  setTimeout(() => {
+    callback(console.log("Making drink"));
+  }, 3000);
+};
+
+const makeFries = () => {
+  console.log("Making fries");
+};
+
+const makeBurger = () => {
+  console.log("Making burger");
+};
+
+const orderMeal = () => {
+  makeBurger();
+  makeDrink(makeFries);
+};
+
+orderMeal();
