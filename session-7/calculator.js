7655b4e1195870ed34f95429eae022c42b3d9847
Ben <benjaminjwalker2910@gmail.com>
2021-10-05 13:27:19 +0100

    prepping for node

---
+++ session-7/calculator.js
@@ -0,0 +1,14 @@
+function add(...nums) {
+  let total = 0;
+  for (let i = 0; i < nums.length; i++) {
+    total += nums[i];
+  }
+  return total;
+}
+
+function divide(num1, num2) {
+  if (num2 === 0) {
+    throw new Error("Can't divide by 0!");
+  }
+  return num1 / num2;
+}
