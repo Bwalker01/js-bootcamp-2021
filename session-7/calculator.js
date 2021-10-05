e5e66b03d4d2de86c8f6ccbf59e0dfc50f0cd0c6
Ben <benjaminjwalker2910@gmail.com>
2021-10-05 13:40:48 +0100

    using modularisation

--- session-7/calculator.js
+++ session-7/calculator.js
@@ -12,3 +12,9 @@ function divide(num1, num2) {
   }
   return num1 / num2;
 }
+
+// console.log(add(1, 2, 3));
+module.exports = {
+  add,
+  divide,
+};
---
+++ session-7/main-calc.js
@@ -0,0 +1,3 @@
+const { add, divide } = require("./calculator");
+console.log("Add using require =>", add(1, 2, 3));
+console.log("Divide using require =>", divide(10, 5));
