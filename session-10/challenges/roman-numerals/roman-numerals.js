f9f3e823dcfb00695c047b4801d2740cdfe5f77a
Ben <benjaminjwalker2910@gmail.com>
2021-10-08 14:38:40 +0100

    romnum challenge

---
+++ session-10/challenges/roman-numerals/roman-numerals.js
@@ -0,0 +1,21 @@
+export const toRomNum = (num) => {
+  const numVals = [
+    ("M", 1000),
+    ("D", 500),
+    ("C", 100),
+    ("L", 50),
+    ("X", 10),
+    ("V", 5),
+    ("I", 1),
+  ];
+
+  let start;
+
+  for (let i = 0; i < numVals.length; i++) {
+    if (numVals[i][1] <= num) {
+      start = i;
+      break;
+    }
+  }
+  return start;
+};
