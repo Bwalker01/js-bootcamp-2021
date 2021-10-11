3585886a2712b3f23d49ba0ab97e059cab3bb9f0
Ben <benjaminjwalker2910@gmail.com>
2021-10-11 14:38:24 +0100

    main for session 10

---
+++ session-11/main.js
@@ -0,0 +1,12 @@
+const messageRE = /hello/;
+
+//Verifies if there is a match and returns true/false
+if (messageRE.test("hello world")) {
+}
+
+const phrase = `The fat cat ran down the street
+It was searching for a mouse to eat`;
+
+const atRE = /at/g;
+
+console.log(atRE.exec(phrase));
