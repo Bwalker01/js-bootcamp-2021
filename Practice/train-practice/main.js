acfc3226a46908971c6b83393e8b0cf2409217fe
Ben <benjaminjwalker2910@gmail.com>
2021-10-18 11:30:58 +0100

    commit tiiiime

---
+++ Practice/train-practice/main.js
@@ -0,0 +1,12 @@
+const submitForm = () => {
+  const title = document.querySelector("#title");
+  if (title === "Default") {
+  }
+  person = {
+    title: document.querySelector("#title"),
+    name: document.querySelector("#name"),
+    email: document.querySelector("#email"),
+    address: document.querySelector("#address"),
+    postCode: document.querySelector("#postCode"),
+  };
+};
